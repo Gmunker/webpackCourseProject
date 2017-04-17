f181538c572672e259ee3f8bb63fadc526a5c85a
Greg Munker <gmunker@gmail.com>
2017-04-16 17:14:33 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Server Routess....
+app.get('/hello', (req, res) => res.send({hi:'there'}));
+
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleWare = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+
+  app.use(webpackMiddleWare(webpack(webpackConfig)));  
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+
+app.listen(process.env.PORT || 3050, () => 
+  console.log('Express Server running on port:3050')
+);
+
+
+
